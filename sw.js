439780a98cc12203f75e167998ff7a255274bb94
lukewmiller44 <lukewmiller44@gmail.com>
2026-06-10 14:01:08 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'bet-that-v1';
+const ASSETS = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // Only cache same-origin requests, skip Firebase/Google requests
+  if (!e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
